fbcb818a3fdc5f0ff3a08979f9cb2eda2987e507
DineshESol <dinesh.s@otomashen.com>
2026-01-27 19:24:46 +0530

    setup project

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    res.status(201).json({ message: "Registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
